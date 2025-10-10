b6518952bf92d7f0d6fcf89888922b7f3527051d
syamjir <rsyamjir@gmail.com>
2025-10-10 22:59:41 +0530

    feat:Add ModeToggle button in layout.tsx for global theme switching

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "@/components/ui/ModeToggle";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased  `}
       >
         <ThemeProvider
           attribute="class"
@@ -34,6 +35,9 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <div className="absolute top-4 right-4 z-50">
+            <ModeToggle />
+          </div>
           {children}
         </ThemeProvider>
       </body>
