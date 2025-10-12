8178465815eb8a37ec5d963a3f5a128f089d52ec
syamjir <rsyamjir@gmail.com>
2025-10-12 22:40:31 +0530

    chore(next.config): add remote image hostnames for flaticon and randomuser

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn-icons-png.flaticon.com",
+      },
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
