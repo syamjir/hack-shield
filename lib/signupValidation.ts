0dcd9f7b740416eea9749dd61d4115a0194b91aa
syamjir <rsyamjir@gmail.com>
2025-10-14 19:17:07 +0530

    feat: add SignupValidation class for email and password validation

---
+++ lib/signupValidation.ts
@@ -0,0 +1,34 @@
+import { Form } from "@/app/auth/signup/page";
+
+export class SignupValidation {
+  private email: string;
+  private password: string;
+  private confirmPassword: string;
+
+  constructor(form: Form, confirmPassword: string) {
+    this.email = form.email;
+    this.password = form.password;
+    this.confirmPassword = confirmPassword;
+  }
+  passwordMatch(): boolean {
+    return !!(
+      this.password &&
+      this.confirmPassword &&
+      this.password === this.confirmPassword
+    );
+  }
+
+  // Validate email format
+  emailValid(): boolean {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(this.email);
+  }
+
+  // Validate password strength
+  passwordValid(): boolean {
+    // At least 8 chars, 1 uppercase, 1 lowercase, 1 number, 1 special character
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return passwordRegex.test(this.password);
+  }
+}
