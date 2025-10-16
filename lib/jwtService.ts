3336c7206a969ed0ed309f7b1c9df0c9bff2e5a7
syamjir <rsyamjir@gmail.com>
2025-10-16 18:27:16 +0530

    feat(auth): add JwtService class for JWT token creation and management

---
+++ lib/jwtService.ts
@@ -0,0 +1,59 @@
+import { IUser } from "@/models/User";
+import { NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
+
+export class JwtService {
+  private user;
+
+  constructor(user: IUser) {
+    this.user = user;
+  }
+
+  private signInToken(id: string, email: string) {
+    return jwt.sign(
+      { id, email },
+      process.env.JWT_SECRET as jwt.Secret,
+      {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+      } as jwt.SignOptions
+    );
+  }
+
+  createSendToken(): NextResponse {
+    const token = this.signInToken(this.user._id, this.user.email);
+
+    const response = NextResponse.json(
+      { message: "Signup successful, , verification code sent", token },
+      { status: 201 }
+    );
+    response.cookies.set("jwt", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      // sameSite: "strict",
+      path: "/",
+      maxAge: Number(process.env.JWT_COOKIE_EXPIRES_IN) * 24 * 60 * 60,
+    });
+
+    return response;
+  }
+
+  async decodeJwtToken(token: string): Promise<{ id: string; email: string }> {
+    return new Promise((resolve, reject) => {
+      jwt.verify(
+        token,
+        process.env.JWT_SECRET as jwt.Secret,
+        (err, decoded) => {
+          if (err || !decoded) {
+            return reject(err || new Error("Invalid token"));
+          }
+
+          const payload = decoded as jwt.JwtPayload & {
+            id: string;
+            email: string;
+          };
+          resolve({ id: payload.id, email: payload.email });
+        }
+      );
+    });
+  }
+}
