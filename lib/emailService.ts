2e6f2004541a187407c82a74f7ee5ab7f7f1cb5e
syamjir <rsyamjir@gmail.com>
2025-10-18 19:28:07 +0530

    feat: implemented OTP email methods for signup and login

---
+++ lib/emailService.ts
@@ -0,0 +1,77 @@
+import nodemailer from "nodemailer";
+import { IUser } from "@/models/User";
+import { htmlToText } from "html-to-text";
+import pug from "pug";
+import path from "path";
+
+class EmailService {
+  private email: string;
+  private firstName: string;
+  private otp: string | null;
+  private url?: string;
+  private from: string;
+
+  constructor(user: IUser, otp?: string, url?: string) {
+    this.email = user.email;
+    this.firstName = user.name?.split(" ")[0] || "User";
+    this.otp = otp || null;
+    this.url = url;
+    this.from = `PassKeeper <${process.env.GMAIL_USER}>`;
+  }
+
+  private newTransport() {
+    return nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_APP_PASSWORD,
+      },
+    });
+  }
+
+  private async sendMailToUser(
+    template: string,
+    subject: string,
+    process: string
+  ) {
+    const templatePath = path.join(
+      __dirname,
+      "../views/email",
+      `${template}.pug`
+    );
+    const html = pug.renderFile(templatePath, {
+      firstName: this.firstName,
+      otp: this.otp,
+      url: this.url,
+      subject,
+      process,
+    });
+
+    const mailOptions = {
+      from: this.from,
+      to: this.email,
+      subject,
+      html,
+      text: htmlToText(html),
+    };
+
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendSignupVerificationOtp() {
+    await this.sendMailToUser(
+      "verificationOtp",
+      "Your PassKeeper verification code (valid for 10 minutes)",
+      "signup"
+    );
+  }
+  async sendLoginVerificationOtp() {
+    await this.sendMailToUser(
+      "verificationOtp",
+      "Your PassKeeper verification code (valid for 10 minutes)",
+      "login"
+    );
+  }
+}
+
+export default EmailService;
