578d2721708a77af9fb991edacb6e9fbe27981a1
syamjir <rsyamjir@gmail.com>
2025-11-10 21:47:42 +0530

    feat(api): add identities API routes

---
+++ app/api/identities/route.ts
@@ -0,0 +1,77 @@
+import { connectToMongo } from "@/lib/connectToMongo";
+import Identity from "@/models/Identity";
+import User from "@/models/User";
+import { IdentityInput, identitySchema } from "@/schemas/identitySchema";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    await connectToMongo();
+    const body = await req.json();
+    const parsed = identitySchema.safeParse(body);
+    if (!parsed.success) {
+      return NextResponse.json(
+        { error: parsed.error.issues[0].message },
+        { status: 400 }
+      );
+    }
+    const {
+      fullName,
+      email,
+      phone,
+      address,
+      city,
+      state,
+      postalCode,
+      country,
+      company,
+      dateOfBirth,
+      notes,
+    }: IdentityInput = parsed.data;
+
+    const userId = req.headers.get("userId");
+    const user = await User.findById(userId);
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
+    // ✅ Create and save new identity entry
+    try {
+      const newIdentity = new Identity({
+        userId: user._id,
+        fullName,
+        email,
+        phone,
+        address,
+        city,
+        state,
+        postalCode,
+        country,
+        company,
+        dateOfBirth,
+        notes,
+      });
+
+      const createdIdentity = await newIdentity.save();
+      return NextResponse.json(
+        {
+          message: "Password saved successfully",
+          data: createdIdentity,
+        },
+        { status: 201 }
+      );
+    } catch (err) {
+      console.error("Database save error:", err);
+      return NextResponse.json(
+        { error: "Failed to save password" },
+        { status: 500 }
+      );
+    }
+  } catch (err) {
+    console.error("Unhandled save Identity error:", err);
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
