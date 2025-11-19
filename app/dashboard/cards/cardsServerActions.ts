2f8b8155108e62b225774e0472e910d7843be65f
syamjir <rsyamjir@gmail.com>
2025-11-19 23:16:24 +0530

    feat(cards-server): update server actions for dashboard cards

--- app/dashboard/cards/cardsServerActions.ts
+++ app/dashboard/cards/cardsServerActions.ts
@@ -1,12 +1,33 @@
-export async function moveNoteToBin(id: string) {
-    const res = await fetch(`/api/cards/${id}/move-to-bin`, {
-      method: "PATCH",
-      headers: { "Content-Type": "application/json" },
-    });
-  
-    const data = await res.json();
-  
-    if (!res.ok) throw new Error(data.error || "Something went wrong");
-    return data;
-  }
-  
+export async function moveCardToBin(id: string) {
+  const res = await fetch(`/api/cards/${id}/move-to-bin`, {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) throw new Error(data.error || "Something went wrong");
+  return data;
+}
+
+export async function retrieveCardNumber(id: string) {
+  const res = await fetch(`/api/cards/${id}/retrieve-cardnumber`, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  });
+
+  const data = await res.json();
+  if (!res.ok) throw new Error(data.error || "Something went wrong");
+  return data;
+}
+
+export async function retrieveCvv(id: string) {
+  const res = await fetch(`/api/cards/${id}/retrieve-cvv`, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  });
+
+  const data = await res.json();
+  if (!res.ok) throw new Error(data.error || "Something went wrong");
+  return data;
+}
