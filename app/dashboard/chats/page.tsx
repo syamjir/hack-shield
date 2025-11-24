e01cb2c6fb9c5801e76834ff94df8632c70b3212
syamjir <rsyamjir@gmail.com>
2025-11-24 19:38:05 +0530

    app/dashboard/chats/: add chats dashboard components

---
+++ app/dashboard/chats/page.tsx
@@ -0,0 +1,41 @@
+import Chat from "@/components/ui/Chat";
+import { cookies } from "next/headers";
+
+async function getLoggedUser(jwt: string) {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/user`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Cookie: `jwt=${jwt}`, // send cookie to server
+    },
+    cache: "no-store", // always fresh
+  });
+
+  const data = await res.json();
+  if (!res.ok) throw new Error(data.error || "Something went wrong");
+  return data;
+}
+
+export default async function UserChatPage() {
+  try {
+    const cookieStore = cookies(); // synchronous
+    const jwt = (await cookieStore).get("jwt")?.value;
+
+    if (!jwt) {
+      return (
+        <div className="p-6 text-red-500">
+          Unauthorized. Please log in first.
+        </div>
+      );
+    }
+    const data = await getLoggedUser(jwt);
+    return <Chat roomId={data.data._id} sender="user" />;
+  } catch (err) {
+    console.error("Error loading chat:", err);
+    return (
+      <div className="p-6 text-red-500">
+        Failed to load chat. {err instanceof Error ? err.message : ""}
+      </div>
+    );
+  }
+}
