1e36d64cd59a00fb432a141ce57ed643dc106b82
syamjir <rsyamjir@gmail.com>
2025-11-25 21:34:05 +0530

    refactor(admin): update DeleteUserButton component

--- app/admin/_components/DeleteUserButton.tsx
+++ app/admin/_components/DeleteUserButton.tsx
@@ -1,7 +1,10 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { deleteUser } from "../adminServerActions";
 import { toast } from "sonner";
+import { useState } from "react";
+import { Loader2, LogIn } from "lucide-react";
 
 export default function DeleteUserButton({
   userId,
@@ -10,26 +13,39 @@ export default function DeleteUserButton({
   userId: string;
   jwt: string;
 }) {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
   const handleDelete = async () => {
     try {
+      setLoading(true);
       const data = await deleteUser(userId, jwt);
       toast.success(data.message);
+      router.push("/admin/users");
     } catch (err) {
       const message =
         err instanceof Error ? err.message : "Something went wrong";
       toast.error(message);
+    } finally {
+      setLoading(false);
     }
-
-    alert("User deleted");
-    window.location.href = "/admin/users";
   };
 
   return (
     <button
       onClick={handleDelete}
-      className="px-4 py-2 bg-red-600 text-white rounded-md"
+      className="px-4 py-2 flex items-center bg-red-600 text-white rounded-md"
     >
-      Delete User
+      {loading ? (
+        <>
+          <Loader2 size={18} className="animate-spin mr-2" />
+          Deleting...
+        </>
+      ) : (
+        <>
+          <LogIn size={18} className="mr-2" />
+          Delete User
+        </>
+      )}
     </button>
   );
 }
