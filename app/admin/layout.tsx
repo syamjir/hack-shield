3a0af7ebe3c87b78ea9e602b62095675f453e21c
syamjir <rsyamjir@gmail.com>
2025-11-26 21:08:10 +0530

    feat(admin): add layout with breadcrumbs and AdminStatsProvider

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -3,6 +3,7 @@
 import { ReactNode } from "react";
 import AdminSidebar from "../admin/_components/AdminSidebar"; // adjust your path
 import { AdminStatsProvider } from "@/contexts/AdminStatsContext";
+import Breadcrumbs from "./_components/Breadcrumbs";
 
 export default function AdminLayout({ children }: { children: ReactNode }) {
   return (
@@ -13,6 +14,7 @@ export default function AdminLayout({ children }: { children: ReactNode }) {
 
         {/* Main Content Wrapper */}
         <main className="flex-1 md:ml-64 px-4 sm:px-6 lg:px-10 pt-20 md:pt-6 pb-10 overflow-y-auto">
+          <Breadcrumbs />
           {children}
         </main>
       </div>
