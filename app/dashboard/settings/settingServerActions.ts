3119979dba89fd13d7dddff53bc88e8045db171a
syamjir <rsyamjir@gmail.com>
2025-11-28 23:43:23 +0530

    feat: implement server actions for dashboard settings

---
+++ app/dashboard/settings/settingServerActions.ts
@@ -0,0 +1,33 @@
+export async function whoAmI(jwt: string) {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/user`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Cookie: `jwt=${jwt}`,
+    },
+    cache: "no-store",
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) throw new Error(data.error || "Something went wrong");
+  return data;
+}
+export async function updatePreference(body: {
+  mode?: string;
+  auto_lock?: boolean;
+}) {
+  const res = await fetch(`/api/user/preference`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+    cache: "no-store",
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) throw new Error(data.error || "Something went wrong");
+  return data;
+}
