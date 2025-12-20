2ea383d6726eaaa1b40ca2d8dd4c7c60efd27179
syamjir <rsyamjir@gmail.com>
2025-12-20 20:47:24 +0530

    test: add PremiumUser component load test

---
+++ __tests__/PremiumUser.test.tsx
@@ -0,0 +1,6 @@
+import { render } from "@testing-library/react";
+import PremiumUser from "../components/ui/PremiumUser";
+
+test("PremiumUser component loads correctly", () => {
+  render(<PremiumUser />);
+});
